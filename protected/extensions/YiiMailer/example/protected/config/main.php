a28edcac130014040d53c408d7a731db26945fb3
unknown <sekar.rohan@LP-1CC1DEB7D599.HCLT.CORP.HCL.IN>
2013-07-31 22:36:44 +0530

    Mailer implementation

--- protected/extensions/YiiMailer/example/protected/config/main.php
+++ protected/extensions/YiiMailer/example/protected/config/main.php
@@ -16,20 +16,19 @@ return array(
 	'import'=>array(
 		'application.models.*',
 		'application.components.*',
-		//Importing my mailer folder in the next line
 		'ext.YiiMailer.YiiMailer',
 	),
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		
+		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'rohan',
-			// If removed, Gii defaults to localhost only. Edit carefully to taste.
+			'password'=>'Enter Your Password Here',
+		 	// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		
+		*/
 	),
 
 	// application components
@@ -39,7 +38,6 @@ return array(
 			'allowAutoLogin'=>true,
 		),
 		// uncomment the following to enable URLs in path-format
-		//REMOVING THE COMMENTS TO SEE IF ROUTING IS WORK
 		/*
 		'urlManager'=>array(
 			'urlFormat'=>'path',
@@ -50,25 +48,23 @@ return array(
 			),
 		),
 		*/
-		/*'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),*/
-		// uncomment the following to use a MySQL database
-		
 		'db'=>array(
-		// I am adding the below code to make the mysql to work
-		//Mysql is running on port 3306 so I am mentioning it and the user_email is my db
-			'connectionString' => 'mysql:host=localhost;port=3306;dbname=user_email',
+			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+		),
+		// uncomment the following to use a MySQL database
+		/*
+		'db'=>array(
+			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '',
 			'charset' => 'utf8',
 		),
-		
+		*/
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
-			'errorAction'=>'site/error',
-		),
+            'errorAction'=>'site/error',
+        ),
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
